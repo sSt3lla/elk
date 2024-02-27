a9ec890d9ef6572f5901cd2b21b1738804a7618a
Ava Rider <st3lla_programming@protonmail.com>
2024-02-28 01:45:55 +1300

    Added max stack size

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
     std::string random_string = fdp.ConsumeRandomLengthString();
     const char *str = random_string.c_str();
     struct js *js = js_create(mem, sizeof(mem));
+    js_setmaxcss(js, 1024);
     js_eval(js, str, strlen(str));
     return 0; 
 }
